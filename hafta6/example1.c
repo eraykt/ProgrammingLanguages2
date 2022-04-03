f14e9076c9afbff2327f744670f14a960ed3da3b
eraykt <alieraykt@gmail.com>
2022-04-03 10:17:32 +0300

    hata fixlendi

--- hafta6/example1.c
+++ hafta6/example1.c
@@ -12,7 +12,7 @@ void main()
     // ptr = [0,1,2,3,4]
 
     printf("%d %d\n", *ptr++, ptr);   // 0 -> ptr adresi 1 arttı [1. indexi tutuyor]
-    printf("%d %d\n", (*ptr)++), ptr; // 1 -> 1 değeri 2 oldu [1. index 2 oldu]
+    printf("%d %d\n", (*ptr)++, ptr); // 1 -> 1 değeri 2 oldu [1. index 2 oldu]
     printf("%d %d\n", *ptr, ptr);     // 2 [1. index]
     printf("%d %d\n", *++ptr, ptr);   // 2 adresi 1 arttır [2. indexi gösterdi]
     printf("%d %d\n", ++*ptr, ptr);   // 3 değeri 1 arttır [2. indexi 1 arttırdı]
